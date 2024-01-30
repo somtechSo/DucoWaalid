80b642f27f4a380e0d2fc862951c365725bb37a4
mohamedabdiahmed <mohamedahmedabdiah@hotmail.com>
2024-01-30 16:12:33 +0300

    Done

--- FrontEnd/src/App.jsx
+++ FrontEnd/src/App.jsx
@@ -1,9 +1,12 @@
 import React from 'react'
+import Main from './Components/Main'
+import Services from './Components/Services'
 
 const App = () => {
   return (
     <div>
-      <h1>Hello World Duco Waalid..</h1>
+      <Main />
+      <Services />ou
     </div>
   )
 }
--- FrontEnd/src/Components/Main.jsx
+++ FrontEnd/src/Components/Main.jsx
@@ -1,10 +1,15 @@
 import React from 'react'
-
+import Back from '../../public/Back.png'
 const Main = () => {
   return (
-    <div>
-      
-    </div>
+    <>
+      <div className="container">
+        <h1 className='translate-y-44 text-3xl text-white'>Kusoo Dhawoow <span>Duco Waalid</span></h1>
+        <div className="image-back -mt-12">
+          <img src={Back} className='bg-cover justify-center ' alt="" />
+        </div>
+      </div>
+    </>
   )
 }
 
