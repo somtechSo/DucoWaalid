32ee657ff68fc62d5af83e2ab89cb3b50da0965d
mohamedabdiahmed <mohamedahmedabdiah@hotmail.com>
2024-01-31 09:29:45 +0300

    Done

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
-export default {
-  content: ["./index.html","./src/**/*.{js,jsx,ts,tsx}"],
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
   theme: {
     extend: {},
   },
